3c70835cc8fa4282ad365317dd1ea5e3c9337668
Daniel Burgner <dburgnerjr@yahoo.com>
2018-10-07 23:45:20 -0500

    Capstone Project Task 2 Complete.
    Implemented UI for each activity and added related entries to
    strings.xml file.  #BuiltByUdacity #GrowWithGoogle #AndroidDev
    #GoogleUdacityScholars

---
+++ app/src/main/java/com/danielburgnerjr/goodforcedemo/GameStartActivity.java
@@ -0,0 +1,13 @@
+package com.danielburgnerjr.goodforcedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameStartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_start);
+    }
+}
