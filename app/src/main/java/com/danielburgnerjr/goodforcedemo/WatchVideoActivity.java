61089031ee765649f903a87dd5faccbdf11fea8b
Daniel Burgner <dburgnerjr@yahoo.com>
2018-11-26 06:53:08 -0600

    Capstone Activity Task 8 Complete.
    Implemented video ads into WatchVideoActivity and ability to start
    them.  #MadeWithUdacity #BuiltByUdacity #AndroidDev #GrowWithGoogle
    #GoogleUdacityScholars

--- app/src/main/java/com/danielburgnerjr/goodforcedemo/WatchVideoActivity.java
+++ app/src/main/java/com/danielburgnerjr/goodforcedemo/WatchVideoActivity.java
@@ -8,20 +8,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.reward.RewardItem;
+import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
 import com.google.gson.Gson;
+import com.google.android.gms.ads.MobileAds;
 
-public class WatchVideoActivity extends AppCompatActivity {
+public class WatchVideoActivity extends AppCompatActivity implements RewardedVideoAdListener {
     private Intent intU;
     private User usrU;
     private Game gmG;
     private Gson gsonG;
     public SharedPreferences preferences;
+    private RewardedVideoAd rvaVideoAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watch_video);
+
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-3940256099942544~3347511713");
         preferences = getSharedPreferences("default", Context.MODE_PRIVATE);
+
+        rvaVideoAd = MobileAds.getRewardedVideoAdInstance(this);
+        loadRewardedVideoAd();
+        rvaVideoAd.show();
 
         intU = getIntent();
         usrU = (User) intU.getSerializableExtra("User");
@@ -62,6 +74,59 @@ public class WatchVideoActivity extends AppCompatActivity {
                 //Toast.makeText(getApplicationContext(), "How To Play", Toast.LENGTH_SHORT).show();
             }
         });
+
+    }
+
+    private void loadRewardedVideoAd() {
+        if (!rvaVideoAd.isLoaded()) {
+            rvaVideoAd.loadAd("ca-app-pub-3940256099942544/5224354917", new AdRequest.Builder().build());
+        }
+    }
+
+    private void startVideoAd(View view) {
+        if (rvaVideoAd.isLoaded()) {
+            rvaVideoAd.show();
+        }
+
+    }
+
+    @Override
+    public void onRewardedVideoAdLoaded() {
+
+    }
+
+    @Override
+    public void onRewardedVideoAdOpened() {
+
+    }
+
+    @Override
+    public void onRewardedVideoStarted() {
+
+    }
+
+    @Override
+    public void onRewardedVideoAdClosed() {
+
+    }
+
+    @Override
+    public void onRewarded(RewardItem rewardItem) {
+
+    }
+
+    @Override
+    public void onRewardedVideoAdLeftApplication() {
+
+    }
+
+    @Override
+    public void onRewardedVideoAdFailedToLoad(int i) {
+
+    }
+
+    @Override
+    public void onRewardedVideoCompleted() {
 
     }
 }
